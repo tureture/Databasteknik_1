e3697671d50aba0da3d805e69e9d8c5cdf6ff94e
Ture Hassler <ture.hassler@gmail.com>
2022-10-05 16:30:22 +0200

    solution to Q7 part 2

---
+++ Q7_products.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+group_number="14" #FILL IN YOUR GROUP NUMBER
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  user="ht22_1_group_"+group_number,
+  passwd="pwd_"+group_number,
+  database="ht22_1_project_group_"+group_number
+)
+
+# Input
+print("Choose product: ")
+product = input()
+print()
+
+mycursor = mydb.cursor()
+
+# Return product and discount
+mycursor.execute("SELECT Prod_title, Discount FROM Product WHERE Prod_title ='" + product + "'")
+myresult = mycursor.fetchall()
+
+# Print current discount, take input for new discount
+print("Product: " + product, "\t Current discount: " + str(myresult[0][1]))
+print("Input new discount: ")
+new_discount = input()
+print()
+
+# Set new discount
+mycursor.execute("UPDATE Product SET Discount=" + new_discount + " WHERE Prod_title ='" + product + "'")
+
+mydb.close()
