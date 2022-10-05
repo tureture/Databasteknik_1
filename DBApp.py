a088848d9b1df7d412e1b02a60dacdea988efc00
Ture Hassler <ture.hassler@gmail.com>
2022-10-05 16:17:43 +0200

    solution to first part of Q7

--- DBApp.py
+++ DBApp.py
@@ -8,16 +8,28 @@ mydb = mysql.connector.connect(
   database="ht22_1_project_group_"+group_number
 )
 
+# Input
 print("Input Department name: ")
 dep = input()
+print()
 
 mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM HasProducts Where HP_Dep_title='" + dep + "'")
 
+# Return all child departments 
+mycursor.execute("SELECT Dep_title FROM Department WHERE Parent_title ='" + dep + "'")
 myresult = mycursor.fetchall()
-print("Users:")
-for x in myresult:
-  print(str(x[0])+"\t"+x[1])
+
+if len(myresult) == 0: # If leaf, print related products
+  mycursor.execute("select Prod_title, Description, Price from ( SELECT Reviews.Prod_title, Description, Price, VAT, Discount, avg(Stars) as avg_rating FROM Product inner join Reviews on Reviews.Prod_title = Product.Prod_title Group by Prod_title ) as T1 where Prod_title in (select HP_Prod_title from HasProducts where HP_Dep_title='" + dep +"');")
+
+  myresult = mycursor.fetchall()
+  print("Products related to department: " + dep)
+  for x in myresult:
+    print(str(x[0]) + "\t" + str(x[1]) + "\t" + str(x[2]))
+  
+else: # Not leaf, print child departments 
+  print("Child departments of " + dep + ":")
+  for row in myresult:
+    print(row[0])
 
 mydb.close()
-
