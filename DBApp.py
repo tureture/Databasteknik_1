f6a67bf395cb71943b169a33e6a793c6d45e43ab
Ture Hassler <ture.hassler@gmail.com>
2022-10-05 13:03:59 +0200

    working example

--- DBApp.py
+++ DBApp.py
@@ -1,21 +1,23 @@
 import mysql.connector
 
-group_number="" #FILL IN YOUR GROUP NUMBER
-
+group_number="14" #FILL IN YOUR GROUP NUMBER
 mydb = mysql.connector.connect(
-  host="groucho.it.uu.se",
-  user="ht21_2_group_"+group_number,
+  host="127.0.0.1",
+  user="ht22_1_group_"+group_number,
   passwd="pwd_"+group_number,
-  database="ht21_2_hotels_group_"+group_number
+  database="ht22_1_project_group_"+group_number
 )
 
+print("Input Department name: ")
+dep = input()
+
 mycursor = mydb.cursor()
-name="\"Golden Nugget\""
-mycursor.execute("SELECT EANHotelID, Name FROM Property WHERE Name = "+name)
+mycursor.execute("SELECT * FROM HasProducts Where HP_Dep_title='" + dep + "'")
 
 myresult = mycursor.fetchall()
-print("EANHotelID\t Name")
+print("Users:")
 for x in myresult:
   print(str(x[0])+"\t"+x[1])
 
 mydb.close()
+
